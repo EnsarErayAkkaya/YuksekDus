312e10ea11a00b248c29d339282eb4cff678d60c
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2020-05-08 14:44:56 +0300

    Started to make anim

--- Assets/Scripts/SwingController.cs
+++ Assets/Scripts/SwingController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class SwingController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]Animator chracterAnimator;
+    public void DoIdle()
     {
-        
+        chracterAnimator.SetBool("isStarted",true);
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            SwingFront();
+        }
+        else if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            SwingBack();
+        }
+        else if(Input.GetKeyDown(KeyCode.Space))
+        {
+            DoIdle();
+        }
+    }
+    public void SwingFront()
+    {
+        chracterAnimator.SetBool("swingFront",true);
+    }
+    public void SwingBack()
+    {
+        chracterAnimator.SetBool("swingFront",false);
     }
 }
